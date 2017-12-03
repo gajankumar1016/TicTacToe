e47e5ecfb2a28d92ebc33adc87f40faf29b693bf
Gajan Kumar <gajankumar1016@gmail.com>
2017-12-03 10:26:03 -0600

    Added constructor, constants, comments, and deleted the Player class.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -3,8 +3,15 @@ public class TicTacToe {
   private static final String PLAYER_1 = "X";
   private static final String PLAYER_2 = "O";
 
+  private static final int NUM_ROWS = 3;
+  private static final int NUM_COLS = 3;
+
   private String[][] board;
   private int turn;
+
+  public TicTacToe() {
+    this.board = new String[NUM_ROWS][NUM_COLS];
+  }
 
   public int getTurn() {
     return turn;
@@ -16,12 +23,12 @@ public class TicTacToe {
   }
 
   public boolean setBoardAt(int row, int col) {
-
+    //this method should utilize the turn number to set the correct player
     return  false;
   }
 
   public String getWinner() {
-
+    //break this up into horizontal, vertical, and diagonal
     return null;
   }
 }
