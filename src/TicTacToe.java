de3379b07bde7ab4e64b773aec2065d178dbd792
Gajan Kumar <gajankumar1016@gmail.com>
2017-12-03 10:18:14 -0600

    Outlined some methods in TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,8 +1,27 @@
 public class TicTacToe {
-  private Player[][] board;
+  //perhaps in the future players could enter their own names to play
+  private static final String PLAYER_1 = "X";
+  private static final String PLAYER_2 = "O";
 
-  private Player getWinner() {
+  private String[][] board;
+  private int turn;
+
+  public int getTurn() {
+    return turn;
+  }
+
+  public String[][] getBoard() {
+    //return copy of board
     return null;
   }
 
+  public boolean setBoardAt(int row, int col) {
+
+    return  false;
+  }
+
+  public String getWinner() {
+
+    return null;
+  }
 }
